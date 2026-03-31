cb33abca97aa26842d68b307f396008b9e7730a7
Nilesh043 <23p65a0508@vbithyd.ac.in>
2026-03-31 11:30:11 +0530

    Add follow model schema for user relationships

---
+++ src/models/follow.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const followSchema = new mongoose.Schema(
+  {
+    follower: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Follower is required"],
+    },
+    followee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Followee is required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
