f74cc35694a101b284ff0b4c8b4bba5fc0a1dd6c
Nilesh043 <23p65a0508@vbithyd.ac.in>
2026-04-01 13:29:28 +0530

    Add InstagramCloneFront frontend project to the repository

---
+++ InstagramCloneFront/src/features/auth/pages/Login.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "../styles/form.scss";
+import { Link } from "react-router";
+import axios from "axios";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+
+    axios
+      .post(
+        "http://localhost:3000/api/auth/login",
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+        },
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
+
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            onInput={(e) => {
+              setUsername(e.target.value);
+            }}
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+          />
+          <input
+            onInput={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <button type="submit">Login</button>
+        </form>
+        <p>
+          Don't have an account?{" "}
+          <Link className="toggleAuthForm" to="/register">
+            Register
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
