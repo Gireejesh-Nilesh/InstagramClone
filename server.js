7a2ac3f0e97d15bc7c8354bdd695c1dece3eeee9
Nilesh043 <23p65a0508@vbithyd.ac.in>
2026-04-01 13:34:29 +0530

    Remove duplicate files from root, keeping them under InstagramClone/

--- server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const app = require("./src/app");
-const connectToDB = require("./src/config/db")
-
-connectToDB();
-
-app.listen(3000, (req, res) => {
-  console.log("Server is running on 3000");
-});
--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectToDB() {
-  await mongoose.connect(process.env.MONGO_URI);
-
-  console.log("Conected to DB");
-}
-
-module.exports = connectToDB;
