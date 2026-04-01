9ff6a0997f6801bc08fbbbb48ceb9d65c4e83505
Nilesh043 <23p65a0508@vbithyd.ac.in>
2026-04-01 17:43:11 +0530

    Add getMeController to retrieve user details and update middleware import

--- InstagramClone/src/controllers/auth.controller.js
+++ InstagramClone/src/controllers/auth.controller.js
@@ -117,7 +117,35 @@ async function registerController(req, res) {
   });
 }
 
+async function getMeController(req, res) {
+  const userId = req.user?.id;
+
+  if (!userId) {
+    return res.status(401).json({
+      message: "Unauthorized Access",
+    });
+  }
+
+  const user = await userModel.findById(userId);
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  res.status(200).json({
+    user: {
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    },
+  });
+}
+
 module.exports = {
   registerController,
   loginController,
+  getMeController,
 };
